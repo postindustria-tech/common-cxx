0802df8836878bf1d2a200e40e17232d4642571b
Steve Ballantine <Stephen@51degrees.com>
2021-09-10 08:28:32 +0000

    Merged PR 5743: TEST: Added dmalloc leak checks to tests.
    Merged PR 5717: TEST: Added dmalloc leak checks to tests.
    
    - TEST: Added dmalloc leak checks to tests.
    - DOC: Added comment and logged stats.
    - BUILD: Add /usr/local to the link and include paths.

--- tests/Base.cpp
+++ tests/Base.cpp
@@ -32,11 +32,20 @@ void Base::SetUp() {
 	// allocation tracking.
 	fiftyoneDegreesMemoryTrackingReset();
 
+#ifdef _DEBUG
 #ifdef _MSC_FULL_VER
 	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
 
 	// Take a sample of the memory before allocating anything.
 	_CrtMemCheckpoint(&_states.s1);
+#else
+	mkdir("dmalloc", 0777);	
+	const ::testing::TestInfo *testInfo = ::testing::UnitTest::GetInstance()->current_test_info();
+	stringstream dmallocArgs;
+	dmallocArgs << "log=dmalloc/" << testInfo->test_case_name() << "." << testInfo->name() << ".log";
+	dmalloc_debug_setup(dmallocArgs.str().c_str());
+	_states.s1 = dmalloc_mark();
+#endif
 #endif
 }
 
@@ -83,10 +92,9 @@ string Base::GetFilePath(string dataFolderName, string fileName) {
  * has not been freed then the test fails.
  */
 void Base::TearDown() {
-#ifdef _MSC_FULL_VER
 #ifndef NDEBUG
+#ifdef _MSC_FULL_VER
 	_CrtMemState s3;
-#endif
 	// Take a sample of the memory now that everything is done.
 	_CrtMemCheckpoint(&_states.s2);
 
@@ -97,6 +105,22 @@ void Base::TearDown() {
 		_CrtMemDumpStatistics(&s3);
 		FAIL() << "There was a memory leak, see debug log for details.";
 	}
+#else
+	_states.s2 = dmalloc_count_changed(_states.s1, 1, 0);
+	if (_states.s2 != 0) {
+		dmalloc_message(
+			"Logging %lu unfreed pointers created during test\n",
+			 _states.s2);
+		dmalloc_log_changed(
+			_states.s2,
+			1, // log unfreed
+			0, // don't log freed
+			1); // log details
+		dmalloc_log_stats();
+		FAIL() << "There was a memory leak, see log file for details.";
+	}
+	dmalloc_shutdown();
+#endif
 #endif
 }
 

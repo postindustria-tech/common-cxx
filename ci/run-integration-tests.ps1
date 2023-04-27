08ede1538d9807d669078e2b49569c1eda92c216
ben51degrees <ben@51degrees.com>
2023-04-27 11:34:51 +0100

    Update run-integration-tests.ps1

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -6,6 +6,23 @@ param(
     [string]$Configuration = "Release"
 )
 
-./cxx/run-integration-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+$RepoPath = [IO.Path]::Combine($pwd, $RepoName, $ProjectDir, "build")
+
+Write-Output "Entering '$RepoPath'"
+Push-Location $RepoPath
+
+try {
+
+    Write-Output "Testing $($Options.Name)"
+
+    ctest -C $Configuration -T test --no-compress-output --output-junit "../test-results/integration/$Name.xml" --tests-regex ".*Integration|Example.*" --exclude-regex ".*OfflineProcessing.*"
+}
+finally {
+
+    Write-Output "Leaving '$RepoPath'"
+    Pop-Location
+
+}
+
 
 exit $LASTEXITCODE

f98a7e33c2ee25a34324bef9ca80a66a4ce3ab78
James Rosewell <james@51degrees.com>
2024-06-07 10:25:03 +0100

    REFACT/DOC: Added documentation and refactored to avoid using fiftyone.h within the json.c.

--- json.h
+++ json.h
@@ -30,7 +30,25 @@
   * JSON methods
   *
   * ## Introduction
-  *
+  * 
+  * Contains common methods to create JSON documents, add properties, add 
+  * either single or list values to properties. String values are escaped to
+  * comply with the JSON specification.
+  * 
+  * ## Data Structures
+  * 
+  * A single data structure is used with members for a) the output buffer, and 
+  * b) the reference data. 
+  * 
+  * The output buffer is represented as a pointer and a length. An additional
+  * member is used to record the number of characters that would be needed to
+  * complete the creation of a valid JSON response. This can be used by the 
+  * caller to increase the buffer size if not big enough and call the related
+  * methods a subsequent time. 
+  * 
+  * Reference data for the property being added, the values being added, and
+  * a collection of strings is also provided.
+  * 
   * @{
   */
 
@@ -44,30 +62,65 @@
 #endif
 #include "property.h"
 #include "string.h"
+#include "list.h"
+#include "data.h"
+#include "collection.h"
+#include "common.h"
 #include "exceptions.h"
-#include "fiftyone.h"
 
+/**
+ * Structure used to populated a JSON string for all required properties and 
+ * values. The implementation will always check to determine if sufficient 
+ * characters remain in the buffer before adding characters. The charsAdded
+ * field is always updated to reflect the number of characters that would be
+ * needed in the buffer if all the values were to be written. This is needed
+ * to determine when the buffer provided needs to be larger.
+ */
 typedef struct fiftyone_degrees_json {
 	char* buffer; // Buffer to add characters to
 	size_t bufferLength; // Length of the buffer
-	size_t charsAdded; // Total characters added over all iterations
+	size_t charsAdded; // Number of characters added or that could be added
 	fiftyoneDegreesCollection* strings; // Collection of strings
-	fiftyoneDegreesProperty* property; // The property being processed
-	fiftyoneDegreesList* values; // The values to be added
+	fiftyoneDegreesProperty* property; // The property being added
+	fiftyoneDegreesList* values; // The values for the property to be added
 	fiftyoneDegreesException* exception;
 } fiftyoneDegreesJson;
 
-EXTERNAL void fiftyoneDegreesJsonDocumentStart(fiftyoneDegreesJson* s);
+/**
+ * Writes the start of the JSON document characters to the buffer in json.
+ * @param json data structure
+ */
+EXTERNAL void fiftyoneDegreesJsonDocumentStart(fiftyoneDegreesJson* json);
 
-EXTERNAL void fiftyoneDegreesJsonDocumentEnd(fiftyoneDegreesJson* s);
+/**
+ * Writes the end of the JSON document characters to the buffer in json.
+ * @param json data structure
+ */
+EXTERNAL void fiftyoneDegreesJsonDocumentEnd(fiftyoneDegreesJson* json);
 
-EXTERNAL void fiftyoneDegreesJsonPropertyStart(fiftyoneDegreesJson* s);
+/**
+ * Writes the start of the property in json->property to the buffer in json.
+ * @param json data structure
+ */
+EXTERNAL void fiftyoneDegreesJsonPropertyStart(fiftyoneDegreesJson* json);
 
-EXTERNAL void fiftyoneDegreesJsonPropertyEnd(fiftyoneDegreesJson* s);
+/**
+ * Writes the end of the property in json->property to the buffer in json.
+ * @param json data structure
+ */
+EXTERNAL void fiftyoneDegreesJsonPropertyEnd(fiftyoneDegreesJson* json);
 
-EXTERNAL void fiftyoneDegreesJsonPropertyValues(fiftyoneDegreesJson* s);
+/**
+ * Writes the values in the json->values list to the buffer in json.
+ * @param json data structure
+ */
+EXTERNAL void fiftyoneDegreesJsonPropertyValues(fiftyoneDegreesJson* json);
 
-EXTERNAL void fiftyoneDegreesJsonPropertySeparator(fiftyoneDegreesJson* s);
+/**
+ * Writes the a property separator to the buffer in json.
+ * @param json data structure
+ */
+EXTERNAL void fiftyoneDegreesJsonPropertySeparator(fiftyoneDegreesJson* json);
 
 /**
  * @}

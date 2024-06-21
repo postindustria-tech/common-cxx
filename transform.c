0e3fb4bda922bb6054902ab930455c314fdb5926
Eugene Dorfman <eugene.dorfman@gmail.com>
2024-06-21 10:59:04 +0200

    FEAT design convert GHEV and SUA to HTTP headers

---
+++ transform.c
@@ -0,0 +1,71 @@
+/* *********************************************************************
+ * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2023 51 Degrees Mobile Experts Limited, Davidson House,
+ * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
+ *
+ * This Original Work is licensed under the European Union Public Licence
+ * (EUPL) v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ *
+ * If using the Work as, or as part of, a network application, by
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading,
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+#include "transform.h"
+
+size_t fiftyoneDegreesTransformIterateGhevFromJson(const char *json,
+                                                            char * const buffer,
+                                                            size_t length,
+                                                            fiftyoneDegreesException * const exception,
+                                                   fiftyoneDegreesTransformCallback callback) {
+    return 0;
+}
+
+size_t fiftyoneDegreesTransformIterateGhevFromBase64(const char *base64,
+                                                              char *buffer,
+                                                              size_t length,
+                                                              fiftyoneDegreesException * const exception,
+                                                     fiftyoneDegreesTransformCallback callback) {
+    return 0;
+}
+
+size_t fiftyoneDegreesTransformIterateSua(const char *json,
+                                                   char *buffer,
+                                                   size_t length,
+                                                   fiftyoneDegreesException * const exception,
+                                          fiftyoneDegreesTransformCallback callback) {
+    return 0;
+}
+
+size_t fiftyoneDegreesTransformGhevFromJson(const char *json,
+                                                     char *buffer,
+                                                     size_t length,
+                                                     fiftyoneDegreesException * const exception,
+                                            fiftyoneDegreesKeyValuePairArray * const headers) {
+    return 0;
+}
+
+size_t fiftyoneDegreesTransformGhevFromBase64(const char *base64,
+                                                       char *buffer,
+                                                       size_t length,
+                                                       fiftyoneDegreesException * const exception,
+                                              fiftyoneDegreesKeyValuePairArray * const headers){
+    return 0;
+}
+
+size_t fiftyoneDegreesTransformSua(const char *json,
+                                            char *buffer,
+                                            size_t length,
+                                            fiftyoneDegreesException * const exception,
+                                   fiftyoneDegreesKeyValuePairArray * const headers) {
+    return 0;
+}

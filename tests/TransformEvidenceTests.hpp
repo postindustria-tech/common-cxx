e7f80e55686df7998207e8eb028dceedd859c71a
Eugene Dorfman <eugene.dorfman@gmail.com>
2024-06-21 10:59:04 +0200

    FEAT draft convert GHEV and SUA to HTTP headers

---
+++ tests/TransformEvidenceTests.hpp
@@ -0,0 +1,13 @@
+//
+//  TransformEvidenceTests.hpp
+//  51DegreesCommon
+//
+//  Created by Eugene Dorfman on 6/17/24.
+//
+
+#ifndef TransformEvidenceTests_hpp
+#define TransformEvidenceTests_hpp
+
+#include <stdio.h>
+
+#endif /* TransformEvidenceTests_hpp */

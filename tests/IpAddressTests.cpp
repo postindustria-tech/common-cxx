111aee38300c29672239ff99eb99d2262fe2e468
Maksym Kucherov <maximus.the0@gmail.com>
2025-01-06 20:09:00 -0500

    Fix warnings? pt.5

--- tests/IpAddressTests.cpp
+++ tests/IpAddressTests.cpp
@@ -111,15 +111,13 @@ TEST(ParseAddress, ParseAddress_Ipv6_LeadingZeros)
 		"The value of the IPv6 address (Leading zeros in each segment) is not correctly parsed.";
 }
 
-TEST(ParseAddress, ParseAddress_Ipv6_MixedIpv6Ipv4)
+TEST(ParseAddress, DISABLED_ParseAddress_Ipv6_MixedIpv6Ipv4)
 {
 	// IPv4-mapped IPv6 address
 	const char * const rawAddress = "::ffff:192.168.1.1";
 	const unsigned char addressBytes[] = {
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 192, 168, 1, 1
 	};
-
-	GTEST_SKIP() << "NOT IMPLEMENTED YET -- IPv4-mapped IPv6 address -- ::ffff:192.168.1.1";
 
 	auto const ipAddress = FiftyoneDegrees::IpIntelligence::IpAddress(rawAddress);
 	EXPECT_TRUE(

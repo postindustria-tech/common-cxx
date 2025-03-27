7a83211a42c591676bfa9e7a3af3efde28c2aeae
Maksym Kucherov <maximus.the0@gmail.com>
2025-03-27 12:40:36 +0200

    CI: Build and test with large data files support.

--- ci/build-project.ps1
+++ ci/build-project.ps1
@@ -10,4 +10,37 @@ param(
 # This is common logic, so let's call the common script
 ./cxx/build-project.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -BuildMethod $BuildMethod
 
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
+Write-Host "`n`n ===== Building large data file tests =====`n`n"
+
+if ($BuildMethod -eq "cmake") {
+
+    ./cxx/build-project.ps1 `
+        -RepoName $RepoName `
+        -ProjectDir $ProjectDir `
+        -Name "$Name-LargeDataFiles" `
+        -BuildDir "build-LargeDataFiles" `
+        -Configuration $Configuration `
+        -BuildMethod $BuildMethod `
+        -ExtraArgs @(
+            "-DLargeDataFileSupport:BOOL=ON"
+        )
+        
+} elseif ($BuildMethod -eq "msbuild") {
+
+    ./cxx/build-project.ps1 `
+        -RepoName $RepoName `
+        -ProjectDir $ProjectDir `
+        -Name "$Name-LargeDataFiles" `
+        -BuildDir "build-LargeDataFiles" `
+        -Configuration "$Configuration-LargeDataFiles" `
+        -BuildMethod $BuildMethod
+
+} else {
+    Write-Error "The build method '$BuildMethod' is not supported."
+}
+
 exit $LASTEXITCODE
